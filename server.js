9b6e6269634169be190647717f1c0c0b90569569
Hoanh An <hoanhan@bennington.edu>
2018-12-10 20:29:05 -0500

    Fix styling

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
+// counter counts the message id.
 counter = 0;
+
+// map holds the socket id and its text color.
 map = {};
 
 server.listen(process.env.PORT || 3000);
-console.log('Server is running...');
+console.log('Server is running on port 3000...');
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
@@ -19,15 +22,15 @@ io.sockets.on('connection', function(socket) {
     console.log(map)
 
     // Remove the socket when user disconnects.
-	socket.on('disconnect', function(data) {
+    socket.on('disconnect', function(data) {
         delete map[socket.id];
-	});
+    });
 
-	// Send message
-	socket.on('send message', function(data) {
+    // Send message.
+    socket.on('send message', function(data) {
         counter += 1;
-		io.sockets.emit('new message', {msgID: counter, msg: data, color: map[socket.id]});
-	});
+	io.sockets.emit('new message', {msgID: counter, msg: data, color: map[socket.id]});
+    });
 });
 
 function getRandomColor() {
